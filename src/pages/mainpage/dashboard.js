aa7c7481c444f3e08c1ea122e431bb43b6ce41c8
vlorj <vlorjan@woit.io>
2023-12-12 10:05:04 +0100

    added a button to fetch data on the mainpage

--- src/pages/mainpage/dashboard.js
+++ src/pages/mainpage/dashboard.js
@@ -1,26 +1,52 @@
 // Dashboard.js
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthContext } from '../../authContext';
 import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
-  const { userData } = useAuthContext();
+  const { userData, refreshAccessToken } = useAuthContext();
   const navigate = useNavigate();
+  const [randomData, setRandomData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const handleGetRandomData = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch('http://139.59.156.28:5080/vlori_program/get_random_data', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${userData.accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setRandomData(data);
+      } else if (response.status === 401) {
+        await refreshAccessToken();
+        handleGetRandomData(); // Retry the request with the new token
+      } else {
+        console.error('Error fetching random data:', response.status);
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    // Check if the user is not authenticated, redirect to login page
     if (!userData.isAuthenticated) {
-      // Delay the redirection to ensure userData is updated
       const redirectTimeout = setTimeout(() => {
         navigate('/');
       }, 0);
 
       return () => clearTimeout(redirectTimeout);
     }
-  }, [userData.isAuthenticated, userData.email, userData.password, userData.accessToken, userData.refreshToken, navigate]);
+  }, [userData.isAuthenticated, navigate]);
 
-  console.log("HEEY BABYY0");
-  console.log(userData.isAuthenticated);
   return (
     <div className="dashboard-container">
       {userData.isAuthenticated ? (
@@ -31,11 +57,37 @@ const Dashboard = () => {
           <p>Access Token: {userData.accessToken}</p>
           <p>Refresh Token: {userData.refreshToken}</p>
 
-          <p>Am i Authenticated?: {userData.isAuthenticated}</p>
-          {/* Add more user data as needed */}
+          <p>Am I Authenticated?: {userData.isAuthenticated}</p>
+
+          {/* Button to trigger the API call */}
+          <button onClick={handleGetRandomData} disabled={loading}>
+            {loading ? 'Fetching Data...' : 'Get Random Data'}
+          </button>
+
+          {/* Display Random Data in a table */}
+          {randomData && (
+            <div>
+              <h2>Random Data:</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Random Number</th>
+                    <th>Random Word</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {randomData.map((item, index) => (
+                    <tr key={index}>
+                      <td>{item.random_number}</td>
+                      <td>{item.random_word}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
       ) : (
-        // You can customize this message or add a redirect to login here as well
         <p>You are not authenticated. Please log in.</p>
       )}
     </div>
